feb601d28957cbb6ee6b23799c11f7bb1f6396af
Mandar <Mandar@LAPTOP-JOV8PT1H>
2023-09-13 18:26:36 +0530

    second Commit

---
+++ DayTwo/src/com/tnsif/daytwo/looping/demo/WhileDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytwo.looping.demo;
+
+public class WhileDemo {
+	public static void main(String[] args) 
+	{
+		int i=90;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
